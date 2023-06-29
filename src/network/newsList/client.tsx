d661ffa4b8dca3bfe8651e2d8fcd93695b046f50
Shintaro1310 <takeshinta6@gmail.com>
2023-06-30 00:42:21 +0900

    とりあえずニュースの取得は終了

--- src/network/newsList/client.tsx
+++ src/network/newsList/client.tsx
@@ -1,54 +1,50 @@
 import axios from "axios";
 import { NewsList, NewsListArticle, NewsListSource } from "./model";
 
-interface NewsListState{
-    setNewsList:({})=>void
-    setNewsListArticle:([])=>void
-    setNewsListSource:({})=>void
+interface NewsListState {
+  setNewsList: (newsList: NewsList | null) => void;
+  setNewsListArticle: (newsListArticle: NewsListArticle[]) => void;
+  setNewsListSource: (newsListSource: NewsListSource | null) => void;
 }
 
-export const fetchNewsList = async({setNewsList,setNewsListArticle,setNewsListSource}:NewsListState)=>{
+export const fetchNewsList = async ({
+  setNewsList,
+  setNewsListArticle,
+  setNewsListSource,
+}: NewsListState) => {
+  try {
+    const response = await axios.get(
+      "https://newsapi.org/v2/top-headlines?country=jp&pageSize=100&apiKey=0849363c5d7343f48aa60aadc2f0f8c5"
+    );
+    const fetchedNewsList: NewsList = {
+      status: response.data.status,
+      totalResults: response.data.totalResults,
+      articles: response.data.articles,
+    };
 
-    try{
-        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=jp&pageSize=100&apiKey=0849363c5d7343f48aa60aadc2f0f8c5")
-        const fetchedNewsList:NewsList={
-            status: response.data.status,
-            totalResults: response.data.totalResults,
-            articles: response.data.articles,
-        }
+    const fetchedNewsListArticle: NewsListArticle[] = Array.isArray(
+      response.data.articles
+    )
+      ? response.data.articles.map((article: any) => ({
+          source: article.source,
+          author: article.author,
+          title: article.title,
+          description: article.description,
+          url: article.url,
+          urlToImage: article.urlToImage,
+          publishedAt: article.publishedAt,
+          content: article.content,
+        }))
+      : [];
 
-        const fetchedNewsListArticle: NewsListArticle[] = Array.isArray(response.data.article)
-        ? response.data.article.map((article: any) => ({
-            source:      article.source,
-            author:      article.author,
-            title:       article.title,
-            description: article.description,
-            url:         article.url,
-            urlToImage:  article.urlToImage,
-            publishedAt: article.publishedAt,
-            content:     article.content,
-
-        }  ))
-         : [];
-
-        const fetchedNewsListSource:NewsListSource={
-            id: response.data.id,
-            name: response.data.name,
-        }
-        setNewsList(fetchedNewsList)
-        setNewsListArticle(fetchedNewsListArticle)
-        setNewsListSource(fetchedNewsListSource)
-
-
-
-
-
-
-    }catch(error){
-        console.log("エラー",error);
-    }
-
-
-
-
-}
+    const fetchedNewsListSource: NewsListSource = {
+      id: null,
+      name: "",
+    };
+    setNewsList(fetchedNewsList);
+    setNewsListArticle(fetchedNewsListArticle);
+    setNewsListSource(fetchedNewsListSource);
+  } catch (error) {
+    console.log("エラー", error);
+  }
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,28 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { useEffect, useState } from 'react'
-import { NewsList, NewsListArticle, NewsListSource } from '@/network/newsList/model'
-import { fetchNewsList } from '@/network/newsList/client'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import { NewsList, NewsListArticle, NewsListSource } from "@/network/newsList/model";
+import { fetchNewsList } from "@/network/newsList/client";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const[newsList,setNewsList]=useState<NewsList|{}>()
-  const[newsListArticle,setNewsListArticle]=useState<NewsListArticle[]>([])
-  const[newsListSource,setNewsListSource]=useState<NewsListSource|{}>()
-  useEffect(()=>{
-    fetchNewsList({setNewsList,setNewsListArticle,setNewsListSource})
-  },[])
-
-
-
+  const [newsList, setNewsList] = useState<NewsList | null>(null);
+  const [newsListArticle, setNewsListArticle] = useState<NewsListArticle[]>([]);
+  const [newsListSource, setNewsListSource] = useState<NewsListSource | null>(null);
+  useEffect(() => {
+    fetchNewsList({ setNewsList, setNewsListArticle, setNewsListSource });
+  }, []);
 
   return (
     <main>
-      <div><button onClick={()=>{console.log(newsList)}}>あ</button>
+      <div>
+      {newsListArticle.map((article) => (
+            <li key={article.source.id}>
+              <h2>{article.title}</h2>
+            </li>
+          ))}
       </div>
-
-
     </main>
-  )
+  );
 }
